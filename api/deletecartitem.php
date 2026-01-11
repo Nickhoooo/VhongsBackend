fe09012bb27945426f08fadc7223e9065efd17dc
Nico Dumayas <nico@example.com>
2026-01-11 13:09:52 +0800

    changing cors

--- api/deletecartitem.php
+++ api/deletecartitem.php
@@ -1,5 +1,13 @@
 <?php
-require 'cors.php';
+header("Access-Control-Allow-Origin: https://vhong-drip.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 include "../config/db.php";
 
 $id = $_POST['id'] ?? null;
--- api/login.php
+++ api/login.php
@@ -1,5 +1,13 @@
 <?php
-require 'cors.php';
+header("Access-Control-Allow-Origin: https://vhong-drip.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 include __DIR__ . "/../config/db.php";
 // ===== SESSION CONFIG =====
 ini_set('session.cookie_samesite', 'None');
